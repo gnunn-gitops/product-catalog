f4040945aedf1f866dea00b6d4708a495bf93286
Gerald Nunn <gerald.b.nunn@gmail.com>
2021-02-22 09:28:15 -0800

    Update siege for prod

--- scripts/siege.sh
+++ scripts/siege.sh
@@ -1,6 +1,6 @@
 COUNT=50
 
-SERVER_HOST=server-product-catalog-dev.apps.home.ocplab.com
+SERVER_HOST=server-product-catalog-prod.apps.home.ocplab.com
 
 siege -r $COUNT -c 50 -v http://${SERVER_HOST}/api/product &
 siege -r $COUNT -c 50 -v http://${SERVER_HOST}/api/product/count &
