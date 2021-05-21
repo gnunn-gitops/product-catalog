e25a8335d817980d5a313f657b4441eb720b7722
Gerald Nunn <gerald.b.nunn@gmail.com>
2021-05-21 13:42:01 -0700

    Refactor cluster naming and bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,8 @@
-kustomize build bootstrap/argocd/applicationset/base | oc apply -f -
+if [ $# -lt 1 ]; then
+    echo "No overlay specified, please specify an overlay from bootstrap/overlays"
+    exit 1
+else
+    OVERLAY=$1
+    echo "Configuring cluster ${OVERLAY}"
+fi
+kustomize build bootstrap/argocd/applicationset/overlays/${OVERLAY} | oc apply -f -
